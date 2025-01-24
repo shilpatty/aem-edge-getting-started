429b79e53003dccc2176d31f1abe19d9988b3d92
shilpatty <138501416+shilpatty@users.noreply.github.com>
2025-01-24 11:55:57 +0000

    Update quote.js

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -5,40 +5,4 @@ export default function decorate(block) {
     blockquote.textContent = quoteWrapper.textContent.trim();
     quoteWrapper.replaceChildren(blockquote);
   
-  
-  {
-    "id": "simpleMarquee",
-    "fields": [
-      {
-        "component": "text",
-        "valueType": "string",
-        "name": "marqueeText",
-        "value": "",
-        "label": "Marquee text",
-        "description": "The text you want shown in your marquee"
-      },
-      {
-        "component": "select",
-        "name": "classes",
-        "value": "",
-        "label": "Background Color",
-        "description": "The marquee background color",
-        "valueType": "string",
-        "options": [
-          {
-            "name": "Red",
-            "value": "bg-red"
-          },
-          {
-            "name": "Green",
-            "value": "bg-green"
-          },
-          {
-            "name": "Blue",
-            "value": "bg-blue"
-          }
-        ]
-      }
-    ]
-  }
 }
